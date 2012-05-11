8ee84647fe36d05f555a569086f21c81641bc4ea
Gianluca Guarini <gianluca.guarini@gmail.com>
2012-05-11 21:35:45 +0200

    code lint

--- js/jQuery.BlackAndWhite.js
+++ js/jQuery.BlackAndWhite.js
@@ -29,160 +29,162 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  * OTHER DEALINGS IN THE SOFTWARE.
  **/
-
-$.fn.extend({
-    BlackAndWhite: function (options) {
-        'use strict';
-        var container = this,
-            self = this,
-            defaults = {
-                hoverEffect: true,
-                webworkerPath: false,
-                responsive: true
-            };
-            options = $.extend(defaults, options);
-        //@public vars
-        var hoverEffect = options.hoverEffect,
-            webworkerPath = options.webworkerPath,
-            responsive = options.responsive;
-
-        //@private var
-        var supportsCanvas = !!document.createElement('canvas').getContext,
-            $window = $(window);
-        /* Check if Web Workers are supported */
-        var supportWebworker = (function () {
-                return (typeof (Worker) !== "undefined") ? true : false;
-            }());
-
-        var isIE7 = $.browser.msie && +$.browser.version === 7;
-
-        //convert any image into B&W using HTML5 canvas
-        function greyImages(img, canvas, width, height) {
-            var ctx = canvas.getContext('2d'),
-                currImg = img,
-                imageData, px, length, i = 0,
-                grey;
-
-            ctx.drawImage(img, 0, 0, width, height);
-
-            var imageData = ctx.getImageData(0, 0, width, height),
-                px = imageData.data,
-                length = px.length;
-
-            // web worker superfast implementation
-            if (supportWebworker && webworkerPath) {
-
-                var BnWWorker = new Worker(webworkerPath + "BnWWorker.js");
-
-                BnWWorker.postMessage(imageData);
-
-                BnWWorker.onmessage = function (event) {
-                    ctx.putImageData(event.data, 0, 0);
+(function ($) {
+    $.fn.extend({
+        BlackAndWhite: function (options) {
+            'use strict';
+            var container = this,
+                self = this,
+                defaults = {
+                    hoverEffect: true,
+                    webworkerPath: false,
+                    responsive: true
                 };
-            } else {
+                options = $.extend(defaults, options);
+            //@public vars
+            var hoverEffect = options.hoverEffect,
+                webworkerPath = options.webworkerPath,
+                responsive = options.responsive;
 
-                // no webworker slow implementation
-                for (; i < length; i += 4) {
-                    grey = px[i] * .3 + px[i + 1] * .59 + px[i + 2] * .11;
-                    px[i] = px[i + 1] = px[i + 2] = grey;
+            //@private var
+            var supportsCanvas = !!document.createElement('canvas').getContext,
+                $window = $(window);
+            /* Check if Web Workers are supported */
+            var supportWebworker = (function () {
+                    return (typeof (Worker) !== "undefined") ? true : false;
+                }());
+
+            var isIE7 = $.browser.msie && +$.browser.version === 7;
+
+            //convert any image into B&W using HTML5 canvas
+            function greyImages(img, canvas, width, height) {
+                var ctx = canvas.getContext('2d'),
+                    currImg = img,
+                    i = 0,
+                    grey;
+
+                ctx.drawImage(img, 0, 0, width, height);
+
+                var imageData = ctx.getImageData(0, 0, width, height),
+                    px = imageData.data,
+                    length = px.length;
+
+                // web worker superfast implementation
+                if (supportWebworker && webworkerPath) {
+
+                    var BnWWorker = new Worker(webworkerPath + "BnWWorker.js");
+
+                    BnWWorker.postMessage(imageData);
+
+                    BnWWorker.onmessage = function (event) {
+                        ctx.putImageData(event.data, 0, 0);
+                    };
+                } else {
+
+                    // no webworker slow implementation
+                    for (; i < length; i += 4) {
+                        grey = px[i] * 0.3 + px[i + 1] * 0.59 + px[i + 2] * 0.11;
+                        px[i] = px[i + 1] = px[i + 2] = grey;
+                    }
+
+                    ctx.putImageData(imageData, 0, 0);
                 }
-
-                ctx.putImageData(imageData, 0, 0);
             }
-        }
 
-        this.init = function (options) {
-            if (supportsCanvas && (!(jQuery.browser.msie && jQuery.browser.version == '9.0'))) {
+            this.init = function (options) {
+                if (supportsCanvas && (!($.browser.msie && $.browser.version == '9.0'))) {
 
-                $(container).each(function (index, currImageWrapper) {
+                    $(container).each(function (index, currImageWrapper) {
 
-                    var pic = $(currImageWrapper).find('img');
-                    var src = $(pic).prop('src');
-                    //getting the Pics proprieties  
+                        var pic = $(currImageWrapper).find('img');
+                        var src = $(pic).prop('src');
+                        //getting the Pics proprieties
 
-                    var currWidth = $(pic).width();
-                    var currHeight = $(pic).height();
+                        var currWidth = $(pic).width();
+                        var currHeight = $(pic).height();
 
-                    //adding the canvas
-                    $('<canvas width="' + currWidth + '" height="' + currHeight + '"></canvas>').prependTo(currImageWrapper);
-                    //getting the canvas
-                    var currCanvas = $(currImageWrapper).find('canvas');
-                    //setting the canvas position on the Pics
-                    $(currCanvas).css({
-                        'position': 'absolute',
-                        top: 0,
-                        left: 0
+                        //adding the canvas
+                        $('<canvas width="' + currWidth + '" height="' + currHeight + '"></canvas>').prependTo(currImageWrapper);
+                        //getting the canvas
+                        var currCanvas = $(currImageWrapper).find('canvas');
+                        //setting the canvas position on the Pics
+                        $(currCanvas).css({
+                            'position': 'absolute',
+                            top: 0,
+                            left: 0
+                        });
+
+                        greyImages(pic[0], currCanvas[0], currWidth, currHeight);
+
+                        if (hoverEffect) {
+                            $(this).mouseenter(function () {
+                                $(this).find('canvas').stop(true, true).fadeOut();
+                            });
+                            $(this).mouseleave(function () {
+                                $(this).find('canvas').stop(true, true).fadeIn();
+                            });
+                        }
+
                     });
 
-                    greyImages(pic[0], currCanvas[0], currWidth, currHeight);
+                } else {
+                    $(container).each(function (index, currImageWrapper) {
+                        var pic = $(currImageWrapper).find('img');
+                        var picSrc = $(currImageWrapper).find('img').prop('src');
+
+                        var currWidth = $(pic).prop('width');
+                        var currHeight = $(pic).prop('height');
+
+                        //adding the canvas
+                        $('<img src=' + picSrc + ' width="' + currWidth + '" height="' + currHeight + '" class="ieFix" /> ').prependTo(currImageWrapper);
+                        $('.ieFix').css({
+                            'position': 'absolute',
+                            top: 0,
+                            left: 0,
+                            'filter': 'progid:DXImageTransform.Microsoft.BasicImage(grayscale=1)'
+                        });
+
+                    });
 
                     if (hoverEffect) {
-                        $(this).mouseenter(function () {
-                            $(this).find('canvas').stop(true, true).fadeOut();
+                        $(container).mouseenter(function () {
+                            $(this).children('.ieFix').stop(true, true).fadeOut();
                         });
-                        $(this).mouseleave(function () {
-                            $(this).find('canvas').stop(true, true).fadeIn();
+                        $(container).mouseleave(function () {
+                            $(this).children('.ieFix').stop(true, true).fadeIn();
                         });
                     }
 
-                })
-
-            } else {
-                $(container).each(function (index, currImageWrapper) {
-                    var pic = $(currImageWrapper).find('img');
-                    var picSrc = $(currImageWrapper).find('img').prop('src');
-
-                    var currWidth = $(pic).prop('width');
-                    var currHeight = $(pic).prop('height');
-
-                    //adding the canvas
-                    $('<img src=' + picSrc + ' width="' + currWidth + '" height="' + currHeight + '" class="ieFix" /> ').prependTo(currImageWrapper);
-                    $('.ieFix').css({
-                        'position': 'absolute',
-                        top: 0,
-                        left: 0,
-                        'filter': 'progid:DXImageTransform.Microsoft.BasicImage(grayscale=1)'
-                    });
-
-                })
-
-                if (hoverEffect) {
-                    $(container).mouseenter(function () {
-                        $(this).children('.ieFix').stop(true, true).fadeOut();
-                    });
-                    $(container).mouseleave(function () {
-                        $(this).children('.ieFix').stop(true, true).fadeIn();
-                    });
                 }
+                if (responsive) {
+                    $window.on('resize orientationchange', container.resizeImages);
+                }
+            };
 
-            }
-            if (responsive) {
-                $window.on('resize orientationchange', container.resizeImages);
-            }
+            this.resizeImages = function () {
+                
+                $(container).each(function (index, currImageWrapper) {
+                    var pic = $(currImageWrapper).find('img:not(.ieFix)');
+                    var currWidth,currHeight;
+                    if (isIE7) {
+                        currWidth = $(pic).prop('width');
+                        currHeight = $(pic).prop('height');
+                    } else {
+                        currWidth = $(pic).width();
+                        currHeight = $(pic).height();
+                    }
+
+                    $(this).find('.ieFix, canvas').css({
+                        width: currWidth,
+                        height: currHeight
+                    });
+
+                });
+            };
+
+            return self.init(options);
+
         }
 
-        this.resizeImages = function () {
-            
-            $(container).each(function (index, currImageWrapper) {
-                var pic = $(currImageWrapper).find('img:not(.ieFix)');
-                if (isIE7) {
-                    var currWidth = $(pic).prop('width');
-                    var currHeight = $(pic).prop('height');
-                } else {
-                    var currWidth = $(pic).width();
-                    var currHeight = $(pic).height();
-                }
-
-                $(this).find('.ieFix, canvas').css({
-                    width: currWidth,
-                    height: currHeight
-                });
-
-            })
-        };
-
-        return self.init(options);
-
-    }
-
-});
+    });
+}(jQuery));
